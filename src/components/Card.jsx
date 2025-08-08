c51b9887787afb6b0a3654c71804efa6fb6b01bc
0BooM <149960425+0BooM@users.noreply.github.com>
2025-08-08 17:10:01 +0200

    Update: Change styles for Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import pokemonImage from "../1.png";
 
 export default function Card() {
   return (
-    <div className="pokemonCard will-change-transform flex flex-col justify-center items-center border-3 rounded-sm py-4 px-8 transition-all duration-200 cursor-pointer hover:scale-110 hover:bg-orange-200 hover:text-black hover:border-orange-400 active:bg-orange-400">
+    <div className="pokemonCard will-change-transform flex flex-col justify-center items-center border-3 rounded-sm py-4 px-8 transition-all duration-200 cursor-pointer hover:scale-110 hover:bg-orange-200 hover:text-black hover:border-orange-400 active:bg-orange-400 active:rotate-4">
       <img src={pokemonImage} alt="Bulbasaur" className="w-25"/>
       <h3 className="text-xl font-bold">Bulbasaur</h3>
     </div>
